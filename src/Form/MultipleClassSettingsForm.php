c6654e1926f9860edb4ab2d30d0c5701120cd969
zohera <zoherasheik@gmail.com>
2023-05-17 17:40:25 +0100

    Add files via upload

--- src/Form/MultipleClassSettingsForm.php
+++ src/Form/MultipleClassSettingsForm.php
@@ -34,28 +34,42 @@ class MultipleClassSettingsForm extends ConfigFormBase {
     $form['#tree'] = TRUE;
 
     if ($num === NULL) {
-      $num = $config->get('add_class') ? count($config->get('add_class')) : 1;
+      $num = $config->get('add_more') ? count($config->get('add_more')) : 1;
       $form_state->set('num_items', $num);
     }
 
     $form['wrapper'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Add Class'),
+      '#title' => $this->t('Add Classes and paths'),
       '#prefix' => '<div id="' . $wrapper_id . '">',
       '#suffix' => '</div>',
+      '#description' => $this->t("Enter multiple classes with space seperated.Specify pages by using their paths. Enter one path per line. The '*' character is a wildcard. An example path is %node-wildcard for every node page. %front is the front page.", [
+        '%node-wildcard' => '/node/*',
+        '%front' => '<front>',
+      ]),
     ];
 
-    $form['wrapper']['add_class'] = [
-      '#type' => 'fieldset',
+    $form['wrapper']['add_more'] = [
+      '#type' => 'table',
+      '#header' => [
+        $this->t('Add one or more classes'),
+        $this->t('Add path'),
+      ],
     ];
 
     for ($i = 0; $i < $num; $i++) {
-      $form['wrapper']['add_class'][$i] = [
+      $form['wrapper']['add_more'][$i] = [
         'extra_class' => [
          '#type' => 'textfield',
-         '#default_value' => $config->get('add_class')[$i]['extra_class'] ?? NULL,
+         '#default_value' => $config->get('add_more')[$i]['extra_class'] ?? NULL,
          '#maxlength' => 256,
-          '#size' => 20,
+         '#size' => 60,
+        ],
+        'extra_path' => [
+          '#type' => 'textfield',
+          '#default_value' => $config->get('add_more')[$i]['extra_path'] ?? NULL,
+          '#maxlength' => 256,
+          '#size' => 40,
         ],
       ];
     }
@@ -90,16 +104,6 @@ class MultipleClassSettingsForm extends ConfigFormBase {
        ];
     }
 
-    $form['specific_pages'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Pages'),
-      '#description' => $this->t("Specify pages by using their paths. Enter one path per line. The '*' character is a wildcard. An example path is %node-wildcard for every node page. %front is the front page.", [
-        '%node-wildcard' => '/node/*',
-        '%front' => '<front>',
-      ]),
-      '#default_value' => $config->get('specific_pages'),
-    ];
-
     return parent::buildForm($form, $form_state);
   }
 
@@ -107,13 +111,12 @@ class MultipleClassSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $specific_pages = $form_state->getValue('specific_pages');
+    $specific_pages = $form_state->getValue('wrapper')['add_more'];
     if (!empty($specific_pages)) {
-      $get_pages = explode(PHP_EOL, $specific_pages);
-      foreach ($get_pages as $values) {
-        $url = explode('/', $values);
-        if (!(str_starts_with($values, '/')) && ($values != '<front>')) {
-          $form_state->setErrorByName('specific_pages', $this->t("@url path needs to start with a slash.", ['@url' => $url]));
+      foreach ($specific_pages as $values) {
+        $url = $values['extra_path'];
+        if (!(str_starts_with($url, '/')) && ($url != '<front>') && (!empty($url))) {
+          $form_state->setErrorByName('add_more', $this->t("@url path needs to start with a slash.", ['@url' => $url]));
         }
       }
     }
@@ -125,9 +128,7 @@ class MultipleClassSettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
     $config = $this->config('multiple_class.settings');
-    $config->set('specific_pages',  $form_state->getValue('specific_pages'));
-    $config->set('add_class', $form_state->getValue('wrapper')['add_class'] ?? []);
-
+    $config->set('add_more', $form_state->getValue('wrapper')['add_more'] ?? []);
     $config->save();
 
     parent::submitForm($form, $form_state);
@@ -136,21 +137,21 @@ class MultipleClassSettingsForm extends ConfigFormBase {
     /**
      * Callback for both ajax-enabled buttons.
      */
-    public function ajaxCallback(array &$form, FormStateInterface $form_state) {
+    public static function ajaxCallback(array &$form, FormStateInterface $form_state) {
       return $form['wrapper'];
     }
 
     /**
      * Submit handler for the "add one" button.
      */
-    public function addOne(array &$form, FormStateInterface $form_state) {
+    public static function addOne(array &$form, FormStateInterface $form_state) {
       $form_state->set('num_items', $form_state->get('num_items') + 1)->setRebuild();
     }
 
     /**
      * Submit handler for the "remove one" button.
      */
-    public function removeOne(array &$form, FormStateInterface $form_state) {
+    public static function removeOne(array &$form, FormStateInterface $form_state) {
       if ($form_state->get('num_items') > 1) {
         $form_state->set('num_items', $form_state->get('num_items') - 1)->setRebuild();
       }
